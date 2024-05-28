3da8ee138918c92b8278bf7955695a0926541328
Veronica <163415343+VeronicaGimenezSantamaria@users.noreply.github.com>
2024-05-28 15:42:55 +0200

    He puesto el metodo para comprobar si hay algun usuario con sesión iniciada y redirigirle a la pantalla correcta en todas las pantallas necesarias

--- TiendaRopa/src/main/java/com/example/tiendaropa/Controlador_altaEdicionEmpleado.java
+++ TiendaRopa/src/main/java/com/example/tiendaropa/Controlador_altaEdicionEmpleado.java
@@ -1,7 +1,9 @@
 package com.example.tiendaropa;
 
 import com.example.tiendaropa.Conexiones.InsercionesBBDD;
+import com.example.tiendaropa.model.Cliente;
 import com.example.tiendaropa.model.Empleado;
+import com.example.tiendaropa.model.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -158,9 +160,21 @@ public class Controlador_altaEdicionEmpleado {
     }
 
     public void mostrarLogin(MouseEvent event) throws IOException {
+        // Usuario,String path y el if hecho por: Vero
+        // Controla que el usuario haya hecho login o lo redirige al login
+        Usuario usuario = Controlador_login.getUsuario();
+        String path;
+        if (usuario instanceof Cliente){// Si el usuario es cliente redirige a su edición
+            path = "FXML_edicionUsuario_Carol.fxml";
+        } else if (usuario instanceof Empleado) {// Si el usuario es empleado redirige a Panel Admin
+            path = "FXML_panelAdmin_Noor.fxml";
+        }else {// En el caso de que no sea ni empleado ni cliente significaria que no ha hecho login
+            path = "FXML_login_Carol.fxml";
+        }
+
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-        Parent root = FXMLLoader.load(getClass().getResource("FXML_login_Carol.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource(path));
 
         stage.setScene(new Scene(root));
         stage.show();
