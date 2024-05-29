fc40ce62c98d6bfb5937a4e7da80f5643294a543
Veronica <163415343+VeronicaGimenezSantamaria@users.noreply.github.com>
2024-05-29 19:09:37 +0200

    He hecho la pantalla y controlador de edicion de cliente. Añadido los métodos necesarios para que la modificacion del cliente funcione y he añadido que el cliente pueda cerrar sesión

--- TiendaRopa/src/main/java/com/example/tiendaropa/Conexiones/InsercionesBBDD.java
+++ TiendaRopa/src/main/java/com/example/tiendaropa/Conexiones/InsercionesBBDD.java
@@ -13,7 +13,7 @@ public class InsercionesBBDD {
         String sql= "INSERT INTO CLIENTE (DNI, nombre, apellidos, telefono, f_nacimiento, direccion, email, pass, " +
                 "saldo_cuenta, num_pedidos, dir_envio, tarjeta_fidelizacion, activo, m_pago) " +
                 "VALUES ('"+dni+"', '"+nombre+"', '"+apellidos+"', '"+telefono+"', '"+fecha+"', '"+direccion+"', '"+
-                email+"', '"+contra+"', 1000.00, 0, '"+direccion+"',"+ fidelizacion+", 1,"+metodoPago+")"; //saldoCuenta es 0!!!!!!!!!!! Att. Noor
+                email+"', '"+contra+"', 0, 0, '"+direccion+"',"+ fidelizacion+", 1,"+metodoPago+")";
         int ok=conn.updateSQL(sql);
         conn.desconectarBBDD();
         return ok;
